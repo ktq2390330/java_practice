a868277ec36831fa4860c951199d39f573d6fc9c
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2024-01-18 15:31:45 +0900

    complete 11-7

--- practice my coding/11-7.java
+++ practice my coding/11-7.java
@@ -1,11 +1,15 @@
-class Student{
-    private int No;
-    private int  japanese;
-    private int math;
-    private int english;
+import java.util.*;
 
-    Student(int N,int jp,int mt,int en){
-        No=N;
+class Student{
+    static String name;
+    static private String No;
+    static private int  japanese;
+    static private int math;
+    static private int english;
+
+    Student(String na,String n,int jp,int mt,int en){
+        name=na;
+        No=n;
         japanese=jp;
         math=mt;
         english=en;
@@ -14,12 +18,40 @@ class Student{
     static double avg(int j,int m,int e){
         return (j+m+e)/3;
     } 
-}
 
-class Main11_7{
-    public static void main(String[] args) {
-        Student Aさん=new Student(001, 89, 65, 88);
-        Student Bさん=new Student(002,80,95,88);
-        Student Cさん=new Student(003, 70, 80, 98);
+    public  String get_name(){
+        return name;
     }
+
+    public String  get_No(){
+        return No;
+    }
+
+    public int get_japanese(){
+        return japanese;
+    }
+
+    int get_mt(){
+        return math;
+    }
+
+    int get_en(){
+        return english;
+    }
+
+
+
+    public static void main(String[] args) {
+        Student A=new Student("A","001", 89, 65, 88);
+        Student B=new Student("B","002",80,95,88);
+        Student C=new Student("C","003", 70, 80, 98);
+        List<String>list=new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        System.out.println(" 名前  学生番号  国語  数学  英語");
+        System.out.println("Aさん   "+A.get_No()+"      "+A.get_japanese()+"    "+A.get_mt()+"    "+A.get_en());
+        System.out.println("Bさん   "+B.get_No()+"      "+B.get_japanese()+"    "+B.get_mt()+"    "+B.get_en());
+        System.out.println("Cさん   "+C.get_No()+"      "+C.get_japanese()+"    "+C.get_mt()+"    "+C.get_en());
+        }
 }
