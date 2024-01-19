0e3f1b2ff415f043ee6a7d7a657ef18a6f7d44b5
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2024-01-19 09:52:50 +0900

    change path

---
+++ practice my coding/11/11-10.java
@@ -0,0 +1,16 @@
+class BookInfo{
+    static private String book_name;
+    static private String writer_name;
+    static private int  year;
+    static private int No;
+
+    BookInfo(String bn,String wn,int y,int no){
+        book_name=bn;
+        No=no;
+        writer_name=wn;
+        year=y;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
