930a536f583b4f02e95276293898c9c289767226
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2024-01-29 12:08:54 +0900

    Main16_2.java

---
+++ practice_my_coding/16/Main16_2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main16_2 {
+    public static void main(String[] args) {
+        String filePath = "practice\\practice_my_coding\\read\\16-2 read.txt";
+        try {
+            FileReader fr = new FileReader(filePath);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
